bcedc91c0dbd7a3b68eff55cd4bd4c0ae59faa40
innovodesign <keith@innovodesign.co.uk>
2019-01-15 12:22:16 +0000

    Add project files.

---
+++ qrlabel/Form1.Designer.cs
@@ -0,0 +1,153 @@
+﻿using System;
+using System.Drawing.Printing;
+
+namespace qrlabel
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.printDocument1 = new System.Drawing.Printing.PrintDocument();
+            this.button1 = new System.Windows.Forms.Button();
+            this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.numericUpDown2 = new System.Windows.Forms.NumericUpDown();
+            this.label1 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // printDocument1
+            // 
+            this.printDocument1.PrintPage += new System.Drawing.Printing.PrintPageEventHandler(this.printDocument1_PrintPage);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(437, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(115, 45);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // numericUpDown1
+            // 
+            this.numericUpDown1.Location = new System.Drawing.Point(33, 24);
+            this.numericUpDown1.Maximum = new decimal(new int[] {
+            40000,
+            0,
+            0,
+            0});
+            this.numericUpDown1.Minimum = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            this.numericUpDown1.Name = "numericUpDown1";
+            this.numericUpDown1.Size = new System.Drawing.Size(70, 22);
+            this.numericUpDown1.TabIndex = 1;
+            this.numericUpDown1.Value = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            this.numericUpDown1.ValueChanged += new System.EventHandler(this.numericUpDown_ValueChanged);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(43, 107);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(500, 500);
+            this.pictureBox1.TabIndex = 2;
+            this.pictureBox1.TabStop = false;
+            // 
+            // numericUpDown2
+            // 
+            this.numericUpDown2.Location = new System.Drawing.Point(147, 24);
+            this.numericUpDown2.Maximum = new decimal(new int[] {
+            10000,
+            0,
+            0,
+            0});
+            this.numericUpDown2.Minimum = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            this.numericUpDown2.Name = "numericUpDown2";
+            this.numericUpDown2.Size = new System.Drawing.Size(70, 22);
+            this.numericUpDown2.TabIndex = 3;
+            this.numericUpDown2.Value = new decimal(new int[] {
+            45,
+            0,
+            0,
+            0});
+            this.numericUpDown2.ValueChanged += new System.EventHandler(this.numericUpDown_ValueChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(43, 70);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(46, 17);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "label1";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(607, 629);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.numericUpDown2);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.numericUpDown1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown2)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+
+        #endregion
+
+        private System.Drawing.Printing.PrintDocument printDocument1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.NumericUpDown numericUpDown1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.NumericUpDown numericUpDown2;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ qrlabel/Form1.cs
@@ -0,0 +1,180 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Drawing.Printing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using ZXing.Common;
+using ZXing;
+using ZXing.QrCode;
+
+namespace qrlabel
+{
+    public partial class Form1 : Form
+    {
+  //      const string prefix = "http://sandbox.innovodesign.co.uk/nvlink2/";
+        const string prefix = "http://find.secontrols.link/nvl2/";
+        public Form1()
+        {
+            InitializeComponent();
+            this.Text = prefix+"NNNN";
+            numericUpDown_ValueChanged(null, null);
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            PCPrint printer = new PCPrint((int)numericUpDown1.Value, (int)numericUpDown2.Value, prefix);
+            
+//            PrintDialog pd = new PrintDialog();
+//            PrinterSettings ps = new PrinterSettings();
+//            pd.PrinterSettings = ps;
+
+            PrintPreviewDialog printPrvDlg = new PrintPreviewDialog();
+            printPrvDlg.Width = 600;
+            printPrvDlg.Height = 1000;
+
+//            DialogResult dr = pd.ShowDialog();
+            printPrvDlg.Document = printer;
+        
+            if (printPrvDlg.ShowDialog() == DialogResult.OK)
+            {
+                printer.Print();
+            }
+        }
+
+        private void printDocument1_PrintPage(object sender, PrintPageEventArgs e)
+        {
+
+        }
+
+        private void numericUpDown_ValueChanged(object sender, EventArgs e)
+        {
+            pictureBox1.Image = PCPrint.makeQR(prefix+numericUpDown1.Value.ToString());
+            bool invalid = (numericUpDown1.Value + numericUpDown2.Value > 4095);
+           if (invalid)
+            {
+                label1.Text = "Only 4096 MAC addresses available\r\nFirmware changes are required and new MAC prefix";
+            }
+            else
+            {
+                label1.Text = prefix + numericUpDown1.Value.ToString();
+            }
+            button1.Enabled = !invalid;
+        }
+
+    }
+
+
+}
+
+
+public class PCPrint : System.Drawing.Printing.PrintDocument
+{
+    const float scale = 100 / 25.4f;
+    const float hpitch = 21.0f;
+    const float vpitch = 20.87f;
+    public string versionString = "V2.00";
+    float originX = 18.14f;
+    float originY = 20.35f+vpitch;
+
+    private int start;
+    private int count;
+    private string prefix;
+    Bitmap logo = new Bitmap("logo.bmp");
+
+    public PCPrint(int start, int count, string prefix) : base()
+    {
+        this.start = start;
+        this.count = count;
+        this.prefix = prefix;
+    }
+
+
+    public static Bitmap makeQR(string s)
+    {
+        QrCodeEncodingOptions options = new QrCodeEncodingOptions
+        {
+            DisableECI = true,
+            CharacterSet = "UTF-8",
+            Width = 500,
+            Height = 500,
+        };
+        var writer = new BarcodeWriter();
+        writer.Format = BarcodeFormat.QR_CODE;
+        writer.Options = options;
+        Bitmap result = new Bitmap(writer.Write(s.Trim()));
+        
+        return result;
+    }
+
+    protected override void OnPrintPage(PrintPageEventArgs e)
+    {
+        // Run base code
+        base.OnPrintPage(e);
+        draw(e.Graphics);
+        e.HasMorePages = false;
+    }
+
+    private void draw(System.Drawing.Graphics canvas)
+    {
+        float X = originX * scale;
+        float Y = originY * scale;
+        int NN = 0;
+        const int labelsPerRow = 9;
+        Pen pen = new Pen(Color.Gray, 4);
+        Font PrinterFont = new Font("Verdana", 6);
+        Font MACfont = new Font("Verdana", 4f);
+
+        //Fit as many characters as we can into the print area      
+        for (int n = start; n < start + count; n++)
+        {
+            float rad = 19f / 2f * scale;
+            canvas.DrawEllipse(new Pen(Color.Gray), X - rad, Y - rad, rad * 2, rad * 2);
+
+            canvas.DrawLine(new Pen(Color.Blue), X - rad, Y, X - rad, Y + vpitch * scale);
+            canvas.DrawLine(new Pen(Color.Blue), X + rad, Y, X + rad, Y + vpitch * scale);
+
+
+            RectangleF rect = new RectangleF(X - 30, Y - 30, 60f, 60f);
+            canvas.DrawImage(PCPrint.makeQR(prefix + n.ToString()), rect);
+
+            string _text = n.ToString();
+            SizeF size = canvas.MeasureString(_text, PrinterFont);
+            StringFormat fmt = new StringFormat(StringFormatFlags.DirectionVertical);
+            canvas.DrawString(_text, PrinterFont, Brushes.Black, X - 38, (int)(Y - size.Width / 2), fmt);
+
+            float Y2 = Y + vpitch * scale;
+            canvas.DrawEllipse(new Pen(Color.Gray), X - rad, Y2 - rad, rad * 2, rad * 2);
+
+
+
+            canvas.DrawImage(logo, new RectangleF(X - 25, Y2 - 30, 50f, 50f / 1.62f));  //aspect ratio 1.62
+
+
+
+            string MAC = "70-B3-D5-2F-C" + (n / 256).ToString("X") + "-" + (n & 255).ToString("X2");
+            size = canvas.MeasureString(MAC, MACfont);
+            canvas.DrawString(MAC, MACfont, Brushes.Black, (int)(X - size.Width / 2), Y2 + 1 * scale);
+
+            size = canvas.MeasureString("NVLink2", PrinterFont);
+            canvas.DrawString("NVLink2", PrinterFont, Brushes.Black, X - size.Width / 2, Y2 + 3 * scale);
+
+            size = canvas.MeasureString(versionString, PrinterFont);
+            canvas.DrawString(versionString, PrinterFont, Brushes.Black, X - size.Width / 2, Y2 + 5 * scale);
+
+            X = X + hpitch * scale;
+            if (++NN == labelsPerRow)
+            {
+                X = X - labelsPerRow * hpitch * scale;
+                Y = Y + vpitch * scale * 2;
+                NN = 0;
+            }
+        }
+    }
+}
+
+
